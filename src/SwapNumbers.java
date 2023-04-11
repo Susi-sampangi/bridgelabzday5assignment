296b3bb5a88138f6a680513ef0db3036f26d7f5b
susi-sampangi <susithrasampangi@gmail.com>
2023-04-12 00:10:09 +0530

    intial version

---
+++ src/SwapNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Before swap: num1 = " + num1 + ", num2 = " + num2);
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("After swap: num1 = " + num1 + ", num2 = " + num2);
+    }
+}
+
